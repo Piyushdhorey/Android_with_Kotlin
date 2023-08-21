5bad72756b45ddcdf1026a455cf05fbd7be18db4
Piyushdhorey <pdhorey@cxr.agency>
2023-08-21 18:08:13 +0530

    frame layout for start page and bound it with ExerciseActivity

---
+++ app/src/main/java/com/example/sevenminuteworkoutapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sevenminuteworkoutapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.sevenminuteworkoutapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+//        val flStart: FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener{
+//            Toast.makeText(this, "Here we will start exercise", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
